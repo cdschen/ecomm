8e1caa497c9348e0a9f3716a8fd689686d8d6e8c
394802198 <394802198@qq.com>
2015-09-13 16:29:11 +1200

    2015-09-13

---
+++ src/main/java/com/sooeez/ecomm/repository/CourierRepository.java
@@ -0,0 +1,9 @@
+package com.sooeez.ecomm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sooeez.ecomm.domain.Courier;
+
+public interface CourierRepository extends JpaRepository<Courier, Long> {
+
+}
