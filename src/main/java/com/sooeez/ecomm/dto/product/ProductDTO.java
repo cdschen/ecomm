e8901e1c43634fbfff256f34cc63445856b46a65
394802198 <394802198@qq.com>
2015-11-22 00:04:11 +1300

    2015-11-21

---
+++ src/main/java/com/sooeez/ecomm/dto/product/ProductDTO.java
@@ -0,0 +1,11 @@
+package com.sooeez.ecomm.dto.product;
+
+import java.io.Serializable;
+
+public class ProductDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String sku;
+	
+}
